845177212a5d8e4d4372932a1480b91f50760da4
Akhil Kumar Gudapuri <akhil35523@gmail.com>
2026-04-28 04:11:53 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 5
+print(type(variable))
+variable = 6.3
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "pool"
+print(type(variable))
+variable = [6.44, False, 'bolo']
+print(type(variable))
