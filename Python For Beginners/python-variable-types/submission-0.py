039846e52de00605f12622cff7d3f8b1221e551b
Akhil Kumar Gudapuri <akhil35523@gmail.com>
2026-04-28 04:09:54 -0700

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 2
+float_type = 2.5
+bool_type = True
+string_type = 'hello'
+list_type = [4, 5.3, True]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
